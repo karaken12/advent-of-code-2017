bd9a71a8c3b45b2a304367ec0c4d25ba75e26104
Tom Potts <karaken12@gmail.com>
2017-12-10 00:47:12 +0000

    Solution for day 1 part two sample inputs

--- 01-captcha/lib/captcha.rb
+++ 01-captcha/lib/captcha.rb
@@ -1,15 +1,20 @@
 class Captcha
   attr_reader :sum
 
-  def initialize(captcha_string)
+  def initialize(captcha_string, halfway=false)
     @captcha_string = captcha_string
+    if halfway
+      @offset = captcha_string.length/2
+    else
+      @offset = 1
+    end
     process_captcha
   end
 
   def process_captcha
     @sum = 0
     for i in 0..(@captcha_string.length-1) do
-      j = (i+1) % @captcha_string.length
+      j = (i+@offset) % @captcha_string.length
       are_equal = @captcha_string[i] == @captcha_string[j]
       if are_equal
         @sum += @captcha_string[i].to_i
--- 01-captcha/spec/captcha_spec.rb
+++ 01-captcha/spec/captcha_spec.rb
@@ -1,32 +1,70 @@
 require 'captcha'
 
 RSpec.describe Captcha do
-  context '1122' do
-    it 'sums to 3' do
-      captcha = Captcha.new('1122')
-      expect(captcha.sum).to eq 3
+  context 'when considering neighbours' do
+    context '1122' do
+      it 'sums to 3' do
+        captcha = Captcha.new('1122')
+        expect(captcha.sum).to eq 3
+      end
+    end
+
+    context '1111' do
+      it 'sums to 4' do
+        captcha = Captcha.new('1111')
+        expect(captcha.sum).to eq 4
+      end
+    end
+
+    context '1234' do
+      it 'sums to 0' do
+        captcha = Captcha.new('1234')
+        expect(captcha.sum).to eq 0
+      end
+    end
+
+    context '91212129' do
+      it 'sums to 9' do
+        captcha = Captcha.new('91212129')
+        expect(captcha.sum).to eq 9
+      end
     end
   end
 
-  context '1111' do
-    it 'sums to 4' do
-      captcha = Captcha.new('1111')
-      expect(captcha.sum).to eq 4
+  context 'when cosidering halfways' do
+    context '1212' do
+      it 'sums to 6' do
+        captcha = Captcha.new('1212', halfway: true)
+        expect(captcha.sum).to eq 6
+      end
+    end
+
+    context '1221' do
+      it 'sums to 0' do
+        captcha = Captcha.new('1221', halfway: true)
+        expect(captcha.sum).to eq 0
+      end
+    end
+
+    context '123425' do
+      it 'sums to 4' do
+        captcha = Captcha.new('123425', halfway: true)
+        expect(captcha.sum).to eq 4
+      end
+    end
+
+    context '123123' do
+      it 'sums to 12' do
+        captcha = Captcha.new('123123', halfway: true)
+        expect(captcha.sum).to eq 12
+      end
+    end
+
+    context '12131415' do
+      it 'sums to 4' do
+        captcha = Captcha.new('12131415', halfway: true)
+        expect(captcha.sum).to eq 4
+      end
     end
   end
-
-  context '1234' do
-    it 'sums to 0' do
-      captcha = Captcha.new('1234')
-      expect(captcha.sum).to eq 0
-    end
-  end
-
-  context '91212129' do
-    it 'sums to 9' do
-      captcha = Captcha.new('91212129')
-      expect(captcha.sum).to eq 9
-    end
-  end
-
 end
