557be285a0b69d4b0ded54509ee4b4c7fc7e6a56
Tom Potts <karaken12@gmail.com>
2017-12-17 22:02:17 +0000

    Parse instructions

--- 08-register/lib/instructions.rb
+++ 08-register/lib/instructions.rb
@@ -1,4 +1,24 @@
 class Instructions
+  Instruction = Struct.new(:register, :increase, :amount, :condition)
+  Condition = Struct.new(:register, :comparison, :constant)
+
   def Instructions.parse(lines)
+    lines.lines.map do |line|
+      Instructions.parse_line(line.strip)
+    end
+  end
+
+  def Instructions.parse_line(line)
+    tokens = line.split(' ')
+    Instruction.new(
+      tokens[0],
+      tokens[1] == 'inc',
+      tokens[2].to_i,
+      Condition.new(
+        tokens[4],
+        tokens[5],
+        tokens[6].to_i
+      )
+    )
   end
 end
--- 08-register/spec/register_spec.rb
+++ 08-register/spec/register_spec.rb
@@ -18,3 +18,26 @@ RSpec.describe Registers do
     end
   end
 end
+
+RSpec.describe Instructions do
+  context 'parse sample program' do
+    instructions = Instructions.parse(
+      "b inc 5 if a > 1
+      a inc 1 if b < 5
+      c dec -10 if a >= 1
+      c inc -20 if c == 10"
+    )
+    it 'should have four instructions' do
+      expect(instructions.size).to eq 4
+    end
+    it 'should have parsed the first instruction' do
+      ins = instructions[0]
+      expect(ins.register).to eq 'b'
+      expect(ins.increase).to be true
+      expect(ins.amount).to eq 5
+      expect(ins.condition.register).to eq 'a'
+      expect(ins.condition.comparison).to eq '>'
+      expect(ins.condition.constant).to eq 1
+    end
+  end
+end
