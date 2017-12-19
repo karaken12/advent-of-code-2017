43d4ecb7384a908522df01ccb1234734498a2700
Tom Potts <karaken12@gmail.com>
2017-12-19 21:46:43 +0000

    Cancel opposites.

--- 11-hex/lib/hex.rb
+++ 11-hex/lib/hex.rb
@@ -5,7 +5,32 @@ class Hex
     end
 
     def simplify
-      @steps
+      steps = {}
+      steps[:ne] = @steps.count('ne')
+      steps[:se] = @steps.count('se')
+      steps[:s ] = @steps.count('s')
+      steps[:sw] = @steps.count('sw')
+      steps[:nw] = @steps.count('nw')
+      steps[:n ] = @steps.count('n')
+
+      cancel = [steps[:n], steps[:s]].min
+      steps[:n] -= cancel
+      steps[:s] -= cancel
+      cancel = [steps[:ne], steps[:sw]].min
+      steps[:ne] -= cancel
+      steps[:sw] -= cancel
+      cancel = [steps[:nw], steps[:se]].min
+      steps[:nw] -= cancel
+      steps[:se] -= cancel
+
+      (
+        ['ne']*steps[:ne] +
+        ['se']*steps[:se] +
+        ['s']*steps[:s] +
+        ['sw']*steps[:sw] +
+        ['nw']*steps[:nw] +
+        ['n']*steps[:n]
+      )
     end
   end
 
--- 11-hex/spec/hex_spec.rb
+++ 11-hex/spec/hex_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe Hex do
         expect(result.size).to eq 3
       end
     end
+
+    context 'ne,ne,sw,sw' do
+      steps = 'ne,ne,sw,sw'
+      it 'is 0 steps away' do
+        path = Hex.parse(steps)
+        result = path.simplify
+        expect(result.size).to eq 0
+      end
+    end
   end
 end
