c546ab14f4a98bd1a462174236eefc78226c669c
Tom Potts <karaken12@gmail.com>
2017-12-23 22:53:26 +0000

    Initial implementation.

---
+++ 10-knot/lib/loop.rb
@@ -0,0 +1,26 @@
+class Loop
+  attr_accessor :elements
+
+  def initialize(loop_size)
+    @elements = *(0..(loop_size-1))
+    @current_position = 0
+  end
+
+  def reverse(length)
+    index = @current_position
+    @elements.rotate!(index)
+    @elements = @elements[0, length].reverse + @elements[length..-1]
+    @elements.rotate!(-index)
+  end
+
+  def hash(lengths)
+    skip = 0
+    @current_position = 0
+    lengths.each do |length|
+      reverse(length)
+      @current_position += length + skip
+      @current_position %= @elements.size
+      skip += 1
+    end
+  end
+end
---
+++ 10-knot/spec/loop_spec.rb
@@ -0,0 +1,49 @@
+require 'loop'
+
+RSpec.describe Loop do
+  context 'part one' do
+    context 'with a small loop' do
+      loop_size = 5
+      context 'with a length of 3' do
+        length = 3
+        it 'becomes 2, 1, 0, 3, 4' do
+          loop1 = Loop.new(loop_size)
+          loop1.reverse(3)
+          expect(loop1.elements).to eq [2, 1, 0, 3, 4]
+        end
+      end
+
+      context 'with lengths of 3, 4' do
+        lengths = [3, 4]
+        it 'becomes 4, 3, 0, 1, 2' do
+          loop1 = Loop.new(loop_size)
+          loop1.hash(lengths)
+          expect(loop1.elements).to eq [4, 3, 0, 1, 2]
+        end
+      end
+
+      context 'with lengths of 3, 4, 1' do
+        lengths = [3, 4, 1]
+        it 'becomes 4, 3, 0, 1, 2' do
+          loop1 = Loop.new(loop_size)
+          loop1.hash(lengths)
+          expect(loop1.elements).to eq [4, 3, 0, 1, 2]
+        end
+      end
+
+      context 'with lengths of 3, 4, 1, 5' do
+        lengths = [3, 4, 1, 5]
+        it 'becomes 3, 4, 2, 1, 0' do
+          loop1 = Loop.new(loop_size)
+          loop1.hash(lengths)
+          expect(loop1.elements).to eq [3, 4, 2, 1, 0]
+        end
+        it 'multiplies to make 12' do
+          loop1 = Loop.new(loop_size)
+          loop1.hash(lengths)
+          expect(loop1.elements[0] * loop1.elements[1]).to eq 12
+        end
+      end
+    end
+  end
+end
