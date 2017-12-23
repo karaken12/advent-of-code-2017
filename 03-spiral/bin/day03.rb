dbc7224ac754c5b9373f944f9472b1bf162323c3
Tom Potts <karaken12@gmail.com>
2017-12-23 11:28:34 +0000

    Convert solution to Rakefile.

--- 03-spiral/bin/day03.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/spiral'
-
-input = 312051
-vector = SpiralMemory.home_vector(input)
-distance = vector.length
-
-puts "Data from square #{input} must be carried #{distance} steps."
